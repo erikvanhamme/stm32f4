262551c1e0f1b6accd87c68f8e0299e2dd78b5e1
Erik Van Hamme <erik.vanhamme@gmail.com>
2015-10-07 11:49:51 +0200

    Updated module.mk to work wit new build script.

--- module.mk
+++ module.mk
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-sources += \
+systemsources += \
 	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c \
 
-ifeq ($(chip),stm32f407)
-sources += stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
+ifeq ($(chip),stm32f407xx)
+systemsources += stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
 endif
 
 # TODO: add support for the other stm32f4 chips if needed (as above). List:
@@ -32,32 +32,7 @@ endif
 #stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f439xx.s
 #stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f446xx.s
 
-systemheaders += \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f415xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f417xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f427xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f437xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f439xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
-	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
-	stm32f4/Drivers/CMSIS/Include/arm_common_tables.h \
-	stm32f4/Drivers/CMSIS/Include/arm_const_structs.h \
-	stm32f4/Drivers/CMSIS/Include/arm_math.h \
-	stm32f4/Drivers/CMSIS/Include/core_cm0.h \
-	stm32f4/Drivers/CMSIS/Include/core_cm0plus.h \
-	stm32f4/Drivers/CMSIS/Include/core_cm3.h \
-	stm32f4/Drivers/CMSIS/Include/core_cm4.h \
-	stm32f4/Drivers/CMSIS/Include/core_cm7.h \
-	stm32f4/Drivers/CMSIS/Include/core_cmFunc.h \
-	stm32f4/Drivers/CMSIS/Include/core_cmInstr.h \
-	stm32f4/Drivers/CMSIS/Include/core_cmSimd.h \
-	stm32f4/Drivers/CMSIS/Include/core_sc000.h \
-	stm32f4/Drivers/CMSIS/Include/core_sc300.h \
+systemincludes += \
+	stm32f4/Drivers/CMSIS/Device/ST/STM32F4xx/Include \
+	stm32f4/Drivers/CMSIS/Include \
 
